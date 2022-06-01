98ad43edfefb786005341ad88e07fc5a63640c38
AsadulloAkramov <asadullo.2807@gmail.com>
2022-06-01 17:41:55 +0500

    some changes added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT;
+console.log(port);
 const app = express();
 app.use(express.static(__dirname + '/dist'))
 
@@ -7,6 +10,6 @@ app.get('', (req, res)=> {
   res.sendFile(__dirname + "dist/index.html ");
 })
 
-app.listen(4200, '0.0.0.0', ()=> {
+app.listen(process.env.PORT || 8000 , '0.0.0.0', ()=> {
   console.log("Application is running on port: 4200");
 })
